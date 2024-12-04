d38f5343413a676ded8ddbf03a313df0466131aa
Daeyang lee <eodid007@gmail.com>
2024-12-05 00:59:09 +0900

    feat: Button 컴포넌트 추가

--- src/components/BookCard/index.tsx
+++ src/components/BookCard/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Button from '../Button';
 
 export default function BookCard({
   title,
@@ -19,7 +20,8 @@ export default function BookCard({
 
       <div className="absolute inset-0 bg-gray-900 bg-opacity-80 opacity-0 transition-opacity duration-300 flex flex-col items-center justify-center text-white group-hover:opacity-100">
         <h1 className="text-lg font-bold">{title}</h1>
-        <h2 className="text-sm">{author}</h2>
+        <h2 className="text-sm mb-[20px]">{author}</h2>
+        <Button>자세히 보기</Button>
       </div>
     </div>
   );
