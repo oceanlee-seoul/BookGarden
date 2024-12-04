9369d34c7d2b3c89d8575834454301748bd1f45d
DAEYANG LEE <eodid007@gmail.com>
2024-12-04 22:03:09 +0900

    Chore: React query 및 axios 세팅 (#1)
    * chore: react version down 19.00 -> 18.3.1
    
    * chore: tanstack query 설치 및 세팅
    
    * chore: axios 설치 및 세팅
    
    * chore: pr template 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,33 @@
-import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
 import { Tables } from '@/types/supabase';
+
+const fetchBooks = async (): Promise<Book[]> => {
+  const { data } = await axios.get('/api/books'); // /api/books로 GET 요청
+  return data;
+};
 
 type Book = Tables<'books'>;
 
 export default function Home() {
-  const [books, setBooks] = useState<Book[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
+  const {
+    data: books,
+    isLoading,
+    error,
+  } = useQuery({ queryKey: ['books'], queryFn: fetchBooks });
 
-  useEffect(() => {
-    const fetchBooks = async () => {
-      try {
-        const response = await fetch('/api/books');
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        setBooks(data);
-      } catch (error) {
-        console.error('Error fetching books:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchBooks();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  if (isLoading) return <div>Loading...</div>;
+  if (error instanceof Error) return <div>Error: {error.message}</div>;
 
   return (
     <div>
-      <h1>Book List</h1>
+      <h1>Books</h1>
       <ul>
-        {books.length === 0 ? (
-          <li>No books available</li>
-        ) : (
-          books.map((book) => (
-            <li key={book.id}>
-              {book.title} by {book.author.join(', ')}
-            </li>
-          ))
-        )}
+        {books?.map((book: { id: number; title: string; author: string[] }) => (
+          <li key={book.id}>
+            {book.title} by {book.author.join(', ')}
+          </li>
+        ))}
       </ul>
     </div>
   );
