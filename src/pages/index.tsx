b506ead9de8d0691831051f59e3a7b1aad00a969
Daeyang lee <eodid007@gmail.com>
2024-12-05 01:12:12 +0900

    feat: main 임시 style 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { useQuery } from '@tanstack/react-query';
 import { Tables } from '@/types/supabase';
 // import BooksForm from '@/components/BooksForm';
 import BookCard from '@/components/BookCard';
+import Button from '@/components/Button';
 
 const fetchBooks = async (): Promise<Book[]> => {
   const { data } = await axios.get('/api/books'); // /api/books로 GET 요청
@@ -22,12 +23,21 @@ export default function Home() {
   if (error instanceof Error) return <div>Error: {error.message}</div>;
 
   return (
-    <div>
-      <div className="grid grid-cols-4 gap-10 max-w-[800px] border p-[40px] rounded-xl bg-white shadow-lg">
+    <div className="max-w-[800px]">
+      <div className="p-[20px] rounded-xl bg-white shadow mb-[15px] flex justify-between">
+        <input
+          className="border"
+          type="text"
+          placeholder="책 이름이나 작가 이름 검색하세요"
+        ></input>
+        <Button>책 추가하기</Button>
+      </div>
+      <div className="grid grid-cols-4 gap-10 border p-[40px] rounded-xl bg-white shadow-lg">
         {books?.slice(0, 8).map((book) => (
           <BookCard key={book.id} title={book.title} author={book.author[0]} />
         ))}
       </div>
+      <div className="text-center mt-[20px]">This is Pagination Section</div>
       {/* <div>
         <BooksForm />
       </div> */}
