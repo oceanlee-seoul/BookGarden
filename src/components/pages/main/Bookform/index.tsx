38d4e1d7b9eaf05bb67f4f12e63f892ff43b167b
Daeyang lee <eodid007@gmail.com>
2025-01-04 19:44:08 +0900

    feat: Input 컴포넌트 추가 및 적용

--- src/components/pages/main/Bookform/index.tsx
+++ src/components/pages/main/Bookform/index.tsx
@@ -8,6 +8,7 @@ import { Book } from '@/types/books';
 import supabase from '@/lib/supabase';
 import useToast from '@/hooks/useToast';
 import { useForm } from 'react-hook-form';
+import Input from '@/components/common/Input';
 
 type BookFormData = {
   title: string;
@@ -107,97 +108,45 @@ const BookForm = ({ initData }: { initData?: Book }) => {
 
   return (
     <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
-      {/* 책 제목 */}
-      <div>
-        <label
-          htmlFor="title"
-          className="block text-sm font-medium text-gray-700"
-        >
-          책 제목
-        </label>
-        <input
-          id="title"
-          {...register('title', { required: '책 제목은 필수입니다.' })}
-          className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:ring-gray-900 focus:border-gray-900"
-        />
-        {errors.title && (
-          <p className="text-red-500 text-sm">{errors.title.message}</p>
-        )}
-      </div>
-
-      {/* 저자 */}
-      <div>
-        <label
-          htmlFor="author"
-          className="block text-sm font-medium text-gray-700"
-        >
-          저자
-        </label>
-        <input
-          id="author"
-          {...register('author', { required: '저자는 필수입니다.' })}
-          className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:ring-gray-900 focus:border-gray-900"
-        />
-        {errors.author && (
-          <p className="text-red-500 text-sm">{errors.author.message}</p>
-        )}
-      </div>
-
-      {/* 출판사 */}
-      <div>
-        <label
-          htmlFor="publisher"
-          className="block text-sm font-medium text-gray-700"
-        >
-          출판사
-        </label>
-        <input
-          id="publisher"
-          {...register('publisher')}
-          className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:ring-gray-900 focus:border-gray-900"
-        />
-      </div>
-
-      {/* 가격과 수량 */}
+      <Input
+        id="title"
+        label="책 제목"
+        errorMessage={errors.title?.message}
+        register={register('title')}
+      />
+      <Input
+        id="author"
+        label="저자"
+        errorMessage={errors.author?.message}
+        register={register('author')}
+      />
+      <Input
+        id="publisher"
+        label="출판사"
+        errorMessage={errors.publisher?.message}
+        register={register('publisher')}
+      />
       <div className="flex gap-4">
         <div className="flex-1">
-          <label
-            htmlFor="price"
-            className="block text-sm font-medium text-gray-700"
-          >
-            가격
-          </label>
-          <input
+          <Input
             id="price"
+            label="가격"
+            errorMessage={errors.price?.message}
             type="number"
-            {...register('price', { required: '가격은 필수입니다.' })}
-            className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:ring-gray-900 focus:border-gray-900"
+            register={register('price')}
           />
-          {errors.price && (
-            <p className="text-red-500 text-sm">{errors.price.message}</p>
-          )}
         </div>
 
         <div className="w-1/3">
-          <label
-            htmlFor="stock"
-            className="block text-sm font-medium text-gray-700"
-          >
-            수량
-          </label>
-          <input
+          <Input
             id="stock"
+            label="수량"
+            errorMessage={errors.stock?.message}
             type="number"
-            {...register('stock', { required: '수량은 필수입니다.' })}
-            className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:ring-gray-900 focus:border-gray-900"
+            register={register('stock')}
           />
-          {errors.stock && (
-            <p className="text-red-500 text-sm">{errors.stock.message}</p>
-          )}
         </div>
       </div>
-
-      {/* 설명 */}
       <div>
         <label
           htmlFor="description"
@@ -228,8 +177,6 @@ const BookForm = ({ initData }: { initData?: Book }) => {
           className="my-2"
         />
       </div>
-
-      {/* 제출 버튼 */}
       <div className="mt-6 flex justify-end">
         <Button type="submit">{initData ? '수정하기' : '추가하기'}</Button>
       </div>
