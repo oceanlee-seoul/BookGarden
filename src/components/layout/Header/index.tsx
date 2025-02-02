93a1c5540e63862d9c06506ad85696baa05faeb8
Daeyang lee <eodid007@gmail.com>
2025-02-02 16:01:57 +0900

    feat: TopNaviagtionBar -> Header로 변경 및 임시 유저 페이지 생성

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,7 +1,7 @@
 import LogoLink from './_components/LogoLink';
 import Menu from './_components/Menu';
 
-export default function TopNavigationBar() {
+export default function Header() {
   return (
     <div className="flex bg-[#FFFFFF] px-[15px] py-[10px] box-border justify-between items-center shadow-lg fixed w-[100vw] z-10">
       <LogoLink />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,20 @@ import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ModalProvider from '@/components/common/Modal';
 import ToastProvider from '@/components/common/Toast';
-import TopNavigationBar from '@/components/layout/TopNavigationBar';
+import Header from '@/components/layout/Header';
+import { useRouter } from 'next/router';
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { pathname } = useRouter();
+  const isAuthPage = pathname === '/sign-in' || pathname === '/sign-up';
+
   return (
     <QueryClientProvider client={queryClient}>
       <ToastProvider />
       <ModalProvider />
-      <TopNavigationBar />
+      {!isAuthPage && <Header />}
       <Component {...pageProps} />
     </QueryClientProvider>
   );
